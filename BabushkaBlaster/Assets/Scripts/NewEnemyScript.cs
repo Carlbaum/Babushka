bb9626b9ad28754e8cc5f4da22e670c2d5686c8f
Carlbaum <oskarcarlbaum@gmail.com>
2016-10-06 01:16:27 +0200

    Setup standard Unity3D project and added some old assets

---
+++ BabushkaBlaster/Assets/Scripts/NewEnemyScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NewEnemyScript : MonoBehaviour {
+
+	int health; 
+	public GameObject grid;
+	bool move = false;
+	Stack<Vector3> toGoTo;
+	Quaternion rotation;
+	public float turnSpeed = 2.0f,moveSpeed =2.0f,startTurn = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if	(move) {
+//			print("Go towards" + toGoTo.Peek());
+			if(Vector3.Distance(transform.position,toGoTo.Peek()) < startTurn) {	
+				toGoTo.Pop();				
+			}	
+			rotation = Quaternion.LookRotation(toGoTo.Peek() - transform.position);
+			transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * turnSpeed);
+			transform.LookAt(toGoTo.Peek());
+			transform.Translate(Vector3.forward * moveSpeed *Time.deltaTime);
+			//transform.FindChild("Camera").LookAt(toGoTo.Peek());
+		
+		} else if(grid.GetComponent<NewGenerateGrid>().foundPath) {
+			move = true;
+			toGoTo = grid.GetComponent<NewGenerateGrid>().getShortestPath();
+			rotation = Quaternion.LookRotation(toGoTo.Peek() - transform.position);
+            print("FOUND PATH!!!!!!!");
+		}	
+		
+		
+	}
+}
---
+++ BabushkaBlaster/Assets/Scripts/NewTileScript.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewTileScript : MonoBehaviour {
+
+	public int tileNumber;
+	public bool accessible;
+
+	int westNeighbour, eastNeighbour, northNeighbour, southNeighbour;
+	int northEastNeighbour, southWestNeighbour, southEastNeighbour, northWestNeighbour;
+
+	Vector2 coordinates;
+	public int hValue, gValue, fValue;
+	public int parentNumber;
+
+	void Start () {
+		
+	}
+
+	public Vector2 getCoordinates(){
+		return coordinates;
+	}
+
+	public void setCoordinates(Vector2 coordVec ){
+		coordinates = coordVec;
+	}
+
+	public void setHValue(int newH ){
+		hValue = newH;
+		transform.Find("hValueText").GetComponent<TextMesh>().text = hValue.ToString();
+	}
+
+	public int getHValue() {
+		return hValue;
+	}
+
+	public void setParentNumber(int newParentNumber ){
+		parentNumber = newParentNumber;
+		transform.Find("parentText").GetComponent<TextMesh>().text = parentNumber.ToString();
+		
+	}
+	
+	public int getParentNumber() {
+		return parentNumber;
+	}
+
+	public void setGValue(int newG ){
+		gValue = newG;
+		transform.Find("gValueText").GetComponent<TextMesh>().text = gValue.ToString();
+	}
+	
+	public int getGValue() {
+		return gValue;
+	}
+
+	public void setFValue(int newF ){
+		fValue = newF;
+		transform.Find("fValueText").GetComponent<TextMesh>().text = fValue.ToString();
+	}
+	
+	public int getFValue() {
+		return fValue;
+	}
+
+	public Vector4[] getAdjacentTilesNumber() {
+		return new Vector4[2]{new Vector4(northNeighbour,eastNeighbour,southNeighbour,westNeighbour), 
+		                      new Vector4(northEastNeighbour, southEastNeighbour, southWestNeighbour, northWestNeighbour)};
+	}
+
+	public void setAdjacent(Vector4 adjacentTiles) {
+		northNeighbour = (int) adjacentTiles.x;
+		eastNeighbour  = (int) adjacentTiles.y;
+		southNeighbour = (int) adjacentTiles.z;
+		westNeighbour  = (int) adjacentTiles.w;
+
+		if (northNeighbour > 0 && eastNeighbour > 0) {
+			northEastNeighbour = northNeighbour + 1;
+		}
+
+		if (southNeighbour > 0 && eastNeighbour > 0) {
+			southEastNeighbour = southNeighbour + 1;
+		}
+		
+		if (southNeighbour > 0 && westNeighbour > 0) {
+			southWestNeighbour = southNeighbour - 1;
+		}
+		
+		if (northNeighbour > 0 && westNeighbour > 0) {
+			northWestNeighbour = northNeighbour - 1;
+		}
+	}
+
+}
